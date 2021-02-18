c5daf23fb2c944285cfc737a6d86bc0e4d14b4bc
Ronildo Souza <ronildo_rns@hotmail.com>
2021-02-18 20:50:38 -0300

    wip

---
+++ assets/js/startup.js
@@ -0,0 +1,12 @@
+// Analytcs
+window.dataLayer = window.dataLayer || [];
+function gtag() { dataLayer.push(arguments); }
+gtag('js', new Date());
+gtag('config', 'UA-150761686-1');
+
+// Clarity
+(function (c, l, a, r, i, t, y) {
+    c[a] = c[a] || function () { (c[a].q = c[a].q || []).push(arguments) };
+    t = l.createElement(r); t.async = 1; t.src = "https://www.clarity.ms/tag/" + i;
+    y = l.getElementsByTagName(r)[0]; y.parentNode.insertBefore(t, y);
+})(window, document, "clarity", "script", "5g4l3vhn24");
